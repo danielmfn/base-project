84ad036ddde7150d08ac103673d730d19046f302
Daniel Pereira de Souza <daniel_mfn@hotmail.com>
2021-10-04 22:04:36 -0300

    chore: exetendendo repositório Produto com QuerydslPredicateExecutor

--- src/main/java/com/praysay/baseproject/repository/ProductRepository.java
+++ src/main/java/com/praysay/baseproject/repository/ProductRepository.java
@@ -2,8 +2,8 @@ package com.praysay.baseproject.repository;
 
 import com.praysay.baseproject.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-//public interface ProductRepository extends JpaRepository<Product, Integer>, QuerydslPredicateExecutor<Product> {
-public interface ProductRepository extends JpaRepository<Product, Integer> {
+public interface ProductRepository extends JpaRepository<Product, Integer>, QuerydslPredicateExecutor<Product> {
 
 }
