900de73eec9ec6ac03afbaa8e490692bee3054e4
Daniel Pereira de Souza <daniel_mfn@hotmail.com>
2021-10-04 22:32:07 -0300

    feat: implementação de consulta com query dsl

--- src/main/java/com/praysay/baseproject/service/ProductService.java
+++ src/main/java/com/praysay/baseproject/service/ProductService.java
@@ -7,7 +7,10 @@ import com.querydsl.core.BooleanBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 public class ProductService {
@@ -19,9 +22,15 @@ public class ProductService {
         BooleanBuilder builder = new BooleanBuilder();
         QProduct qProduct = QProduct.product;
 
-//        builder.and(qProduct.price. > new BigDecimal("3"));
+        builder.and(qProduct.price.gt(new BigDecimal("3")));
 
-        return productRepository.findAll();
+        return builder.getValue() != null ? getProductsFromInterable(builder) : productRepository.findAll();
+    }
+
+    private List<Product> getProductsFromInterable(BooleanBuilder builder) {
+        assert builder.getValue() != null;
+        return StreamSupport.stream(productRepository.findAll(builder.getValue()).spliterator(), false)
+                .collect(Collectors.toList());
     }
 
 }
